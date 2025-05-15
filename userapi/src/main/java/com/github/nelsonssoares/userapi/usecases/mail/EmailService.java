ead5e966084ca284feda74fcd5f73f4c095f1934
Nelson <punkpunkada@gmail.com>
2025-05-15 19:59:43 -0300

    adding email with attachment mathod in EmailService

--- userapi/src/main/java/com/github/nelsonssoares/userapi/usecases/mail/EmailService.java
+++ userapi/src/main/java/com/github/nelsonssoares/userapi/usecases/mail/EmailService.java
@@ -1,10 +1,16 @@
 package com.github.nelsonssoares.userapi.usecases.mail;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.nelsonssoares.userapi.commons.configs.email.EmailConfig;
 import com.github.nelsonssoares.userapi.domain.dtos.request.EmailRequestDTO;
 import com.github.nelsonssoares.userapi.mail.EmailSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +24,28 @@ public class EmailService {
                 .withSubject(emailRequest.getSubject())
                 .withMessage(emailRequest.getBody())
                 .send(emailConfig);
+    }
+
+    public void sendEmailWithAttachment(String emailRequestJson, MultipartFile file) {
+        File tempFile = null;
+        try {
+            EmailRequestDTO emailRequest = new ObjectMapper().readValue(emailRequestJson, EmailRequestDTO.class);
+            tempFile = File.createTempFile("attachment", file.getOriginalFilename());
+            file.transferTo(tempFile);
+
+            emailSender.to(emailRequest.getTo())
+                    .withSubject(emailRequest.getSubject())
+                    .withMessage(emailRequest.getBody())
+                    .setAttachment(tempFile.getAbsolutePath())
+                    .send(emailConfig);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error parsing email request", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error processing the attachment", e);
+        } finally {
+            if (tempFile != null && tempFile.exists()) tempFile.delete();
+        }
     }
 
 }
