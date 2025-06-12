3ec4b87c9a28bf394b22bd5097b172b01db10d97
Nelson <punkpunkada@gmail.com>
2025-06-12 12:20:52 -0300

    Circuit Breaker is hard to work at Gateway, is better use only Feign

--- Authentication-Gateway/src/main/java/com/github/nelsonssoares/AuthGateway/service/impl/UserResgistrationServiceImpl.java
+++ Authentication-Gateway/src/main/java/com/github/nelsonssoares/AuthGateway/service/impl/UserResgistrationServiceImpl.java
@@ -9,10 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.io.Serializable;
 
 @RequiredArgsConstructor
 @Service
@@ -21,9 +18,7 @@ public class UserResgistrationServiceImpl implements UserResgistrationService {
     private static final Logger log = LoggerFactory.getLogger(UserResgistrationServiceImpl.class);
 
     private final UserGateway userGateway;
-    private final KafkaTemplate<String , Serializable> template;
 
-    @CircuitBreaker(name = "registrationCB", fallbackMethod = "registerUserFallback")
     @Override
     public ResponseEntity<UserRequest> registerUser(UserRequest userRequest) throws Exception {
 
@@ -35,15 +30,5 @@ public class UserResgistrationServiceImpl implements UserResgistrationService {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
-    public ResponseEntity<UserRequest> registerUserFallback(UserRequest userRequest, Throwable t) {
-        log.error("Fallback triggered for user registration: {}", t.getMessage());
 
-        if(template.send("registration-topic", userRequest).isDone()){
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(userRequest);
-        } else {
-            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(userRequest);
-
-        }
-
-    }
 }
